121ece4e9bfe2adb85afc97012cfcd247735bde8
renoke <renaud.kern@gmail.com>
2010-02-02 11:01:41 +0100

    Shouldafy DynAttributes tests.

--- test/unit/dyn_attributes_test.rb
+++ test/unit/dyn_attributes_test.rb
@@ -3,10 +3,11 @@ require 'test_helper'
 class DynDummy < ActiveRecord::Base
   before_save :set_dummy_node_id
   set_table_name 'versions'
-  #include Zena::Use::DynAttributes::ModelMethods
-  #dynamic_attributes_setup :nested_alias => {%r{^d_(\w+)} => ['dyn']}
-  include Dynamo::Core
-  include Dynamo::Serialization::Marshal
+
+  include Dynamo::Attribute
+  dynamo :color, String
+  dynamo :life, String
+  dynamo :shoes, String
 
   def set_dummy_node_id
     self[:node_id] = 0
@@ -14,29 +15,8 @@ class DynDummy < ActiveRecord::Base
   end
 end
 
-class DynStrictDummy < ActiveRecord::Base
-  set_table_name 'versions'
-  # include Zena::Use::DynAttributes::ModelMethods
-  # dynamic_attributes_setup :only => [:bio, :phone], :nested_alias => {%r{^d_(\w+)} => ['dyn']}
-  include Dynamo::Core
-  include Dynamo::Serialization::Marshal
-
-  def before_save
-    self[:node_id] = 123
-    self[:user_id] = 123
-  end
-end
-
-class DynSubStrictDummy < DynStrictDummy
-end
-
-class DynSub2StrictDummy < DynStrictDummy
-  include Zena::Use::DynAttributes::ModelMethods
-  dynamic_attributes_setup :only => [:hell]
-end
-
-class AttrDummy < ActiveRecord::Base
-  set_table_name 'dyn_attributes'
+class ChildDummy < DynDummy
+  dynamo :age, Numeric
 end
 
 class DynAttributesTest < Test::Unit::TestCase
@@ -49,117 +29,119 @@ class DynAttributesTest < Test::Unit::TestCase
     should_create :dyn_dummy
   end
 
-  def test_simple
-    assert record = DynDummy.create(:title => 'this is my title', :text=>'', :comment=>'', :summary=>'')
-    assert_nil record.dyn['color']
-    record.dyn['color'] = 'blue'
-    assert_equal 'blue', record.dyn['color']
-    assert_nil record.dyn[:color]
-    assert record.save
+  context 'Simple test' do
+    setup do
+      @record = DynDummy.create(:title => 'this is my title', :text=>'', :comment=>'', :summary=>'', :shoes=>'worn')
+    end
+    subject {@record}
 
-    record = DynDummy.find(record[:id]) # reload
-    assert_equal 'blue', record.dyn['color']
-  end
+    should 'create and read dynamo on record' do
+      assert_nil subject.dyn[:color]
+      subject.dyn[:color] = 'blue'
+      assert_equal 'blue', subject.dyn[:color]
+      assert_save subject
+      subject.reload
+      assert_equal 'blue', subject.dyn[:color]
+    end
 
-  def test_find
-    record = DynDummy.create(:title => 'worn-shoes', :text=>'', :comment=>'', :summary=>'', :color=>'blue', :life=>'fun', :shoes=>'worn')
-    assert record = DynDummy.find(record)
-    assert_equal 'worn', record.dyn[:shoes]
-  end
+    should 'load dynamo after find' do
+      record = DynDummy.find(subject)
+      assert_equal 'worn', record.dyn[:shoes]
+    end
 
-  def test_update_attributes
-    assert record = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :color=>'blue', :life=>'fun', :shoes=>'worn')
-    assert record.update_attributes(:life => 'hell', :heidegger => 'Martin')
-    assert_equal 'Martin', record.dyn[:heidegger]
-    assert_equal 'blue', record.dyn[:color]
-  end
+    should 'update attributes' do
+      subject.update_attributes(:title=>'lolipop', :life=>'fun')
+      assert_equal 'lolipop', subject.title
+      assert_equal 'worn', subject.dyn[:shoes]
+      assert_equal 'fun', subject.dyn[:life]
+    end
 
-  def test_delete
-    assert record = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :d_color=>'blue', :d_life=>'fun', :d_shoes=>'worn')
-    proxy = record.dyn
-    keys = proxy.instance_variable_get(:@keys)
-    assert record.update_attributes(:d_life => nil)
-    assert_nil record.dyn['life']
-    assert_nil AttrDummy.find_by_id(keys['life'])
-  end
+    should 'save updated attributes' do
+      subject.update_attributes(:title=>'lolipop', :life=>'fun')
+      assert subject.save
+      subject.reload
+      assert_equal 'lolipop', subject.title
+      assert_equal 'worn', subject.dyn[:shoes]
+      assert_equal 'fun', subject.dyn[:life]
+    end
 
-  def test_delete_many
-    assert record = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :color=>'blue', :life=>'fun', :shoes=>'worn')
-    assert record.update_attributes(:life => nil, :shoes => nil)
-    assert_nil record.dyn[:life]
-    assert_nil record.dyn[:shoes]
-  end
+    should 'delete attribute' do
+      subject.dyn.delete :shoes
+      assert_nil subject.dyn[:shoes]
+      subject.save
+      subject.reload
+      assert_nil subject.dyn[:shoes]
+    end
 
-  def test_each
-    assert record = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :d_color=>'blue', :d_life=>'fun', :d_shoes=>'worn')
-    record.dyn.each do |k,v|
-      case k
-      when 'color'
-        assert_equal 'blue', v
-      when 'life'
-        assert_equal 'fun', v
-      when 'shoes'
-        assert_equal 'worn', v
+    should 'delete attributes with update_attributes' do
+      subject.update_attributes(:life => nil, :shoes => nil)
+      assert_nil subject.dyn[:life]
+      assert_nil subject.dyn[:shoes]
+    end
+
+    should 'each itarate on object dynamos' do
+      subject.update_attributes(:color=>'blue', :life=>'fun', :shoes=>'worn')
+      subject.dyn.each do |k,v|
+        case k
+        when 'color'
+          assert_equal 'blue', v
+        when 'life'
+          assert_equal 'fun', v
+        when 'shoes'
+          assert_equal 'worn', v
+        end
       end
     end
   end
 
-  def test_dyn_equal
-    assert record  = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :color=>'blue', :life=>'fun', :shoes=>'worn')
-    assert record2 = DynDummy.create(:title => 'hulk', :text=>'', :comment=>'', :summary=>'', :lobotomize=>'me')
-    record2.dyn = record.dyn
-    record2.save
+  context 'Advanced test' do
+    should 'replace exchange dynamo between object' do
+      record  = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :color=>'blue', :life=>'fun', :shoes=>'worn')
+      record2 = DynDummy.create(:title => 'hulk', :text=>'', :comment=>'', :summary=>'', :lobotomize=>'me')
+      record2.dyn = record.dyn
+      record2.save
+      assert_equal 'blue', record2.dyn[:color]
+      assert_nil record2.dyn[:lobotomize]
+    end
 
-    record2 = DynDummy.find(record2[:id]) # reload
-    assert_equal 'blue', record2.dyn[:color]
-    assert_nil record2.dyn[:lobotomize]
+    should 'replace dynamo' do
+      assert record  = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :color=>'blue', :life=>'fun', :shoes=>'worn')
+      record.dyn = {:color => 'yellow'}
+      record.save
+      assert_equal 'yellow', record.dyn[:color]
+      assert_nil record.dyn[:life]
+      assert_nil record.dyn[:shoes]
+    end
+
+    should 'destroy object with dynamos' do
+      record = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :life=>'fun')
+      n = DynDummy.count
+      assert record.destroy
+      assert record.frozen?
+      assert_equal n-1, DynDummy.count
+    end
+
+    should 'change object' do
+      record = DynDummy.create(:title => 'this is my title', :text=>'', :comment=>'', :summary=>'', :color=>'red', :life => 'in love')
+       assert !record.changed?
+       record.update_attributes(:color=>'black')
+       assert !record.changed?
+       record.dyn[:life] = 'in depression'
+       assert record.changed?
+    end
   end
 
-  def test_dyn_update_with
-    assert record  = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :color=>'blue', :life=>'fun', :shoes=>'worn')
-    record.dyn = {:color => 'yellow', :lobotomize=>'me'}
-    record.save
+  context 'Wiht inheritence' do
+    should 'child class be able to create parent dynamos' do
+      assert record = ChildDummy.create(:title => 'this is my title', :text=>'', :comment=>'', :summary=>'', :color=>'red', :life => 'in love')
+      assert_equal 'red', record.dyn[:color]
+    end
 
-    record = DynDummy.find(record[:id]) # reload
-    assert_equal 'yellow', record.dyn[:color]
-    assert_equal 'me', record.dyn[:lobotomize]
-    assert_nil record.dyn[:life]
-    assert_nil record.dyn[:shoes]
+    should 'child class be able to own dynamos' do
+      assert record = ChildDummy.create(:title => 'this is my title', :text=>'', :comment=>'', :summary=>'', :color=>'red', :age => 10)
+      assert_equal 'red', record.dyn[:color]
+      assert_equal 10, record.dyn[:age]
+    end
   end
 
-  def test_set_with_hash
-    assert record  = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'')
-    record.dyn = {'fingers' => 'hurt'}
-    assert record.save, "Can save"
-
-    record = DynDummy.find(record[:id]) # reload
-    assert_equal 'hurt', record.dyn['fingers']
-  end
-
-  def test_delete
-    assert record  = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :life=>'fun', :joy=>'weird')
-    assert_equal 'weird', record.dyn.delete(:joy)
-    assert_nil record.dyn[:joy]
-    assert record.save
-    record = DynDummy.find(record) # reload
-    assert_nil record.dyn[:joy]
-    assert_equal 'fun', record.dyn[:life]
-  end
-
-  def test_destroy
-    record = DynDummy.create(:title => 'lolipop', :text=>'', :comment=>'', :summary=>'', :life=>'fun', :joy=>'weird')
-    n = DynDummy.count
-    assert record.destroy
-    assert record.frozen?
-    assert_equal n-1, DynDummy.count
-  end
-
-  def test_changed
-    record = DynDummy.create(:title => 'this is my title', :text=>'', :comment=>'', :summary=>'', :bio=>'biography', :hell => 'blind love')
-    assert !record.changed?
-    record.attributes={:bio=>'biography'}
-    assert !record.changed?
-    record.attributes={:bio=>'Gemüse'}
-    assert record.changed?
-  end
 end
