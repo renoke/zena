8cae6c6fd5ed59258c57c1dc2a55a086936d41c7
renoke <renaud.kern@gmail.com>
2010-03-23 17:41:34 +0100

    Change default destination in unzip method to use the zip file directory instead.

--- lib/zena/use/data_compression.rb
+++ lib/zena/use/data_compression.rb
@@ -5,13 +5,14 @@ module Zena
   module Use
     module DataCompression
 
-      # Inflate a zip file in a destination folder, which is by default the current directory.
+      # Inflate a zip file in a destination folder, which is by default the zip_file directory.
       # It create destination directory for each file included in the zip file.
       #
       #   Zena::Use::DataCompression.unzip('blue_template.zip', 'skin')
       #
-      def self.unzip(zip_file, destination=Dir.pwd)
-        Zip::ZipFile.new(zip_file).each do |f|
+      def self.unzip(zip_file, destination=nil)
+        destination ||= File.dirname(zip_file)
+        ::Zip::ZipFile.new(zip_file).each do |f|
           f_path = File.join(destination, f.name)
           FileUtils.mkdir_p(File.dirname(f_path))
           f.extract(f_path) unless File.exist?(f_path)
